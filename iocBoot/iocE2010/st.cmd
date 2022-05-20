e6b65d33d2fd9e438bcc2baf23ea3159bc34803e
Peiyu Quan <41603034+PeiyuQuan@users.noreply.github.com>
2022-05-19 17:53:02 -0700

    Update st.cmd

--- iocBoot/iocE2010/st.cmd
+++ iocBoot/iocE2010/st.cmd
@@ -15,24 +15,25 @@ E2010_registerRecordDeviceDriver pdbbase
 epicsEnvSet ("STREAM_PROTOCOL_PATH", "${TOP}/E2010App/Db")
 epicsEnvSet ("PREFIX", "SLAC:E2010:")
 epicsEnvSet ("PORT", "serial1")
-
+epicsEnvSet ("M", "zirox1:")
 #- Set this to see messages from mySub
 #var mySubDebug 1
-drvAsynSerialPortConfigure("serial1", "/dev/ttyUSB0", 0, 0, 0)
+drvAsynIPPortConfigure("serial1", "192.168.0.35:4002<192.168.0.35:4002/> COM", 0, 0, 0)
+#drvAsynSerialPortConfigure("serial1","/dev/ttyUSB0",0,0,0)
 asynOctetSetInputEos("serial1",0,"\r")
 asynOctetSetOutputEos("serial1",0,"\r")
 asynSetOption("serial1",0,"baud","9600")
 asynSetOption("serial1",0,"bits","8")
 asynSetOption("serial1",0,"stop","1")
 asynSetOption("serial1",0,"parity","none")
-asynSetOption("serial1",0,"clocal","N")
+#asynSetOption("serial1",0,"clocal","Y")
 asynSetOption("serial1",0,"crtscts","N")
 
 
 #- Run this to trace the stages of iocInit
 #traceIocInit
 
-dbLoadRecords("${TOP}/E2010App/Db/ZIROXE2010.db","P=$(PREFIX),PORT=serial1")
+dbLoadRecords("${TOP}/E2010App/Db/ZIROXE2010.db","P=$(PREFIX),M=$(M),PORT=serial1")
 dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=SLAC:E2010:")
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX),R=asyn1,PORT=serial1,ADDR=0,IMAX=80,OMAX=80")
 
